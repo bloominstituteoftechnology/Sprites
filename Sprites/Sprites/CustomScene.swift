018ef2ac0a0409567d0afb5514971bce7b4f6108
Frulwinn <frulwinn@gmail.com>
2018-11-05 15:52:51 -0800

    added color range to nodes

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -3,12 +3,17 @@ import SpriteKit
 
 class CustomScene: SKScene {
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Fetch a touch or leave
         guard !touches.isEmpty, let touch = touches.first
             else { return }
         
         let node = SKShapeNode(circleOfRadius: 8)
-        node.fillColor = .red
+        //node.fillColor = .red
+        node.fillColor = UIColor(red: .random(in: 0.5...1.0), green: .random(in: 0.5...1.0), blue: .random(in: 0.5...1.0), alpha: 1.0)
         node.position = touch.location(in: self)
         addChild(node)
+        
+
     }
 }
