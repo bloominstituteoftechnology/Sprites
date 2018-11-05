c37ed6c9033f11edc735ca591926a935a3d46b4c
Austin Cole <austincole@MacBook-Pro.local>
2018-11-05 15:14:48 -0700

    Replace red color of dots with randomly generated color

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -13,8 +13,18 @@ class CustomScene: SKScene {
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard !touches.isEmpty, let touch = touches.first
             else { return }
+        
+        //Return a random color - drand48() returns a random double between 0 and 1 - we convert the double into a float by type casting the result of drand48 to CGFloat, because UIColor() can only take a float
+        func getRandomColor() -> UIColor {
+            let randomRed: CGFloat = CGFloat(drand48())
+            let randomGreen: CGFloat = CGFloat(drand48())
+            let randomBlue: CGFloat = CGFloat(drand48())
+            return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        }
+        
+        
         let node = SKShapeNode(circleOfRadius: 8)
-        node.fillColor = .red
+        node.fillColor = getRandomColor()
         node.position = touch.location(in: self)
         addChild(node)
         
