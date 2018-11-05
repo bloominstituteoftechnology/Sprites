32548888ffc84730845ecba2f3e9e2c9ae4e940c
Frulwinn <frulwinn@gmail.com>
2018-11-05 13:59:01 -0800

    added child node and node specs

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -1,1 +1,14 @@
-import Foundation
+import UIKit
+import SpriteKit
+
+class CustomScene: SKScene {
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first
+            else { return }
+        
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .red
+        node.position = touch.location(in: self)
+        addChild(node)
+    }
+}
