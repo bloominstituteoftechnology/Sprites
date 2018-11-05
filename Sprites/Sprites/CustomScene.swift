b7f76af5960d5abf0d67a32caf41d2017645bf1c
Sean Doyle <sdoylelambda@gmail.com>
2018-11-05 17:35:08 -0500

    attemped stretch goals

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -1,6 +1,13 @@
 import UIKit
 import SpriteKit
 
+// random color
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
 class CustomScene: SKScene {
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard !touches.isEmpty, let touch = touches.first
@@ -11,5 +18,13 @@ class CustomScene: SKScene {
         node.fillColor = .red
         node.position = touch.location(in: self)
         addChild(node)
-    }
+        
+        //random color
+        
+       
+        
+        //zooms the node to 130% of its original size and returns the node back
+        let zoomAction = SKAction.scale(by: 1.3, duration: 0.3)
+        let unzoomAction = SKAction.scale(to: 1.0, duration: 0.1)
+        let sequenceAction = SKAction.sequence([zoomAction, zoomAction, unzoomAction])    }
 }
