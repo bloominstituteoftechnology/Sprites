33ac7a5803a4371c79b4778100a0b710ced49d61
Frulwinn <frulwinn@gmail.com>
2018-11-05 13:51:58 -0800

    added skview and created an outlet for it

--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -1,18 +1,13 @@
-//
-//  ViewController.swift
-//  Sprites
-//
-//  Created by Lambda_School_Loaner_34 on 11/5/18.
-//  Copyright © 2018 Frulwinn. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var skview: SKView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
