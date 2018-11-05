5a7f0f4fa32f65288734bcf1eb2e6e42bff7b673
Vijay Das <vijaytdas@gmail.com>
2018-11-05 15:43:55 -0600

    add basic functionality

---
+++ Sprites/Sprites/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Sprites
+//
+//  Created by Vijay Das on 11/5/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+    @IBOutlet var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
+}
