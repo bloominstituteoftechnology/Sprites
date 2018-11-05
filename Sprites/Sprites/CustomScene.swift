2df7f9d651ca4c9a516cecb926fb0b6b930d2ae6
Ben Hakes <bhhakes@gmail.com>
2018-11-05 17:02:40 -0600

    implement basic app functionality

---
+++ Sprites/Sprites/CustomScene.swift
@@ -0,0 +1,23 @@
+//
+//  CustomScene.swift
+//  Sprites
+//
+//  Created by Benjamin Hakes on 11/5/18.
+//  Copyright © 2018 Benjamin Hakes. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class CustomScene: SKScene {
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first
+            else { return }
+        
+        // Work with your touch here
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .red
+        node.position = touch.location(in: self)
+        addChild(node)
+    }
+}
