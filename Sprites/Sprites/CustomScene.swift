a5fcc955b9a342b2a998cc9a47840aec12ac9530
Meera Andersen <meera.andersen@gmail.com>
2018-11-05 17:27:19 -0700

    Changed color to Random on click

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -1,6 +1,15 @@
 import UIKit
 import SpriteKit
 
+func randomColor() -> UIColor {
+    let range = 0.5...256.0 as ClosedRange<CGFloat>
+    return UIColor (red: CGFloat.random(in: range),
+                    green: CGFloat.random(in: range),
+                    blue: CGFloat.random(in: range),
+                    alpha: 2)
+}
+
+
 class CustomScene: SKScene {
     
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -8,10 +17,10 @@ class CustomScene: SKScene {
             else { return }
         
         let node = SKShapeNode(circleOfRadius: 8)
-        node.fillColor = .red
+        node.fillColor = randomColor()
         node.position = touch.location(in: self)
         addChild(node)
         
-        // Work with your touch here
+        
     }
 }
