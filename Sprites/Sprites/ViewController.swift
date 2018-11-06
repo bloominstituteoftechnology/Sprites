9dc073c9229b82ce7acb56f3d870f221eb12ef38
Stuart Sippel <stuartsippel@icloud.com>
2018-11-06 08:40:54 -0600

    Add xcodeproj

---
+++ Sprites/Sprites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sprites
+//
+//  Created by Stuart on 11/6/18.
+//  Copyright © 2018 Stuart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
