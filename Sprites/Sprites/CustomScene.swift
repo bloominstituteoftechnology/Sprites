3743008317f5ce294d127cd1a7f077b16bd4bdf8
Rob Herold <rderek@bresnan.net>
2018-11-06 12:35:49 -0700

    latest attempt to reupload

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -1,9 +1,16 @@
-//
-//  CustomScene.swift
-//  Sprites
-//
-//  Created by Rob Herold on 11/5/18.
-//  Copyright © 2018 Rob Herold. All rights reserved.
-//
+import UIKit
+import SpriteKit
 
-import Foundation
+class CustomScene: SKScene {
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first
+            else { return }
+        
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .red
+        node.position = touch.location(in: self)
+        addChild(node)
+        
+        // Work with your touch here
+    }
+}
