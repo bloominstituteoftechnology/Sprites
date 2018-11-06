628d78eb0e096c9ff30a6b3ddfadc4b431030be4
Frulwinn <frulwinn@gmail.com>
2018-11-05 16:03:43 -0800

    added 130 zoom and return back to original size

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -9,10 +9,16 @@ class CustomScene: SKScene {
             else { return }
         
         let node = SKShapeNode(circleOfRadius: 8)
-        //node.fillColor = .red
         node.fillColor = UIColor(red: .random(in: 0.5...1.0), green: .random(in: 0.5...1.0), blue: .random(in: 0.5...1.0), alpha: 1.0)
         node.position = touch.location(in: self)
         addChild(node)
+        
+        // Playing with zoom
+        
+        let zoomAction = SKAction.scale(by: 1.3, duration: 0.3)
+        let unzoomAction = SKAction.scale(to: 1.0, duration: 0.1)
+        let sequenceAction = SKAction.sequence([zoomAction, unzoomAction])
+        node.run(sequenceAction)
         
 
     }
