c9750312f00d94200d7af69b1d6f579c2d3053d0
Audrey Welch <welch.audreyl@gmail.com>
2018-11-13 18:04:55 -0500

    Stretch Goal #3 - Nodes appear in center of screen and move to location of touch

--- Sprites MBP/Sprites MBP/CustomScene.swift
+++ Sprites MBP/Sprites MBP/CustomScene.swift
@@ -21,7 +21,8 @@ class CustomScene: SKScene {
         node.fillColor = .red
         
         // placement of node
-        node.position = touch.location(in: self)
+        node.position = CGPoint(x: frame.midX, y: frame.midY)
+        //node.position = touch.location(in: self)
         addChild(node)
         
         // randomize color of node each time it's touched
@@ -35,7 +36,15 @@ class CustomScene: SKScene {
         // zoom and unzoom node when touched
         let zoomAction = SKAction.scale(by: 1.3, duration: 0.3)
         let unzoomAction = SKAction.scale(to: 1.0, duration: 0.1)
-        let sequenceAction = SKAction.sequence([zoomAction, unzoomAction])
+        
+        // create move action
+        let position = touch.location(in: self)
+        let actionDuration = 1.0
+        let moveAction = SKAction.move(to: position, duration: actionDuration)
+        
+        // put all zooming and moving actions into a sequence to happen consecutively
+        let sequenceAction = SKAction.sequence([zoomAction, moveAction, unzoomAction])
+        // run the sequence
         node.run(sequenceAction)
     }
 }
