b1d07e9658c1948b5f989381f24802305bbae0f9
Sean Doyle <sdoylelambda@gmail.com>
2018-11-13 15:29:39 -0500

    view controller added

--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -1,20 +1,16 @@
-//
-//  ViewController.swift
-//  Sprites
-//
-//  Created by Lambda_School_Loaner_18 on 11/13/18.
-//  Copyright © 2018 Lambda_School_Loaner_18. All rights reserved.
-//
-
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
-
-
 }
-
