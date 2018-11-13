f2bafb73a2e95489757af579b3f06904d54cdaf8
Rob Herold <rderek@bresnan.net>
2018-11-13 15:54:24 -0700

    had to use git add to stage changes for commit

--- CustomScene.swift
+++ CustomScene.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2018 Rob Herold. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+
+class CustomScene: SKScene {
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first
+            else { return }
+        
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .red
+        node.position = touch.location(in: self)
+        addChild(node)
+    }
+}
--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -1,20 +1,14 @@
-//
-//  ViewController.swift
-//  Sprites
-//
-//  Created by Rob Herold on 11/13/18.
-//  Copyright © 2018 Rob Herold. All rights reserved.
-//
-
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
-
-
 }
-
