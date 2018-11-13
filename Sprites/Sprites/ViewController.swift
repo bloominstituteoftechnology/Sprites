88162866b3973adfbb692f8de3d2662c427a1d73
Vijay Das <vijaytdas@gmail.com>
2018-11-13 15:01:20 -0600

    add CustomScene class, SpriteView and single color persistent node function (MVP for project)

--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -1,7 +1,20 @@
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var skview: SKView!
+    
+
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
