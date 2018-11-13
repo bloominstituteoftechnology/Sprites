191269143db2e7f535b13969970ca175e39da32e
Meera Andersen <meera.andersen@gmail.com>
2018-11-13 14:25:13 -0700

    add zoom/unzoom node sequence.

--- Sprites Project/Sprites Project/CustomScene.swift
+++ Sprites Project/Sprites Project/CustomScene.swift
@@ -7,16 +7,24 @@ class CustomScene: SKScene {
         guard !touches.isEmpty, let touch = touches.first
             else { return }
         
-        
+     
         let position = touch.location(in: self)
-        let actionDuration = 1.0
-        let moveAction = SKAction.move(to: position, duration: actionDuration)
-        
-        
+       
+       
+
         let node = SKShapeNode(circleOfRadius: 8)
         node.fillColor = randomColor()
         node.position = touch.location(in: self)
         addChild(node)
+        
+        
+        
+        let zoomAction = SKAction.scale(by: 1.3, duration: 0.5)
+        let unzoomAction = SKAction.scale(to: 1.0, duration: 0.3)
+        let sequenceAction = SKAction.sequence([zoomAction, unzoomAction])
+        node.run(sequenceAction)
+        
+      
         
     }
     
@@ -25,7 +33,7 @@ class CustomScene: SKScene {
         let green = CGFloat.random(in: 0.0 ... 1.0)
         let blue = CGFloat.random(in: 0.0 ... 1.0)
         
-        return UIColor(red: red, green: green, blue: green, alpha: 1.0)
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
 
