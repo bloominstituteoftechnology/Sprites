927b80d7a645d9ecd271eac181c00438f771f664
Rob Herold <rderek@bresnan.net>
2018-11-13 15:33:12 -0700

    initial commit after rebuild due to fatal system error

---
+++ CustomScene.swift
@@ -0,0 +1,9 @@
+//
+//  CustomScene.swift
+//  Sprites
+//
+//  Created by Rob Herold on 11/13/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import Foundation
--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -7,26 +7,14 @@
 //
 
 import UIKit
-import SpriteKit
 
 class ViewController: UIViewController {
-    @IBOutlet var skview: SKView!
-    
-    var skscene: CustomScene? = nil
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        skscene = CustomScene(size: view.bounds.size)
-        skview.presentScene(skscene)
-    }
-    
-}
 
-    func randomColor() -> UIColor {
-        let red = CGFloat.random(in: 0.25...0.75)
-        let green = CGFloat.random(in: 0.25...0.75)
-        let blue = CGFloat.random(in: 0.25...0.75)
-        
-        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
+
+
+}
 
