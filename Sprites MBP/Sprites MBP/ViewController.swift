1d3fa461b3533aeb522b6d85b02b1189f6fb50ac
Audrey Welch <welch.audreyl@gmail.com>
2018-11-13 16:40:31 -0500

    Implement basic app functionality that creates red circle nodes when the screen is touched

--- Sprites MBP/Sprites MBP/ViewController.swift
+++ Sprites MBP/Sprites MBP/ViewController.swift
@@ -1,18 +1,30 @@
 //
 //  ViewController.swift
 //  Sprites MBP
-//
-//  Created by Audrey Seversen on 11/13/18.
-//  Copyright © 2018 Audrey Welch. All rights reserved.
-//
+
 
 import UIKit
+import SpriteKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var skview: SKView!
+    
+    // assign the variable skscene to the type of CustomScene (if it contains anything) and set it to nil
+    var skscene: CustomScene? = nil
+    
+    // add the scene into the view when it is opened
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
