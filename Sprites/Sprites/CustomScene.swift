029fc968963ab1a9224320cfdab6cca86c9ed6e5
Rob Herold <rderek@bresnan.net>
2018-11-13 14:38:05 -0700

    functioning app, trying to get colors to randomize

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -1,9 +1,20 @@
-//
-//  CustomScene.swift
-//  Sprites
-//
-//  Created by Rob Herold on 11/13/18.
-//  Copyright © 2018 Rob Herold. All rights reserved.
-//
+import UIKit
+import SpriteKit
 
-import Foundation
+class CustomScene: SKScene {
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first
+            else { return }
+        
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .red
+        node.position = touch.location(in: self)
+        addChild(node)
+
+
+// Zoom action
+        let zoomAction = SKAction.scale(by: 1.3, duration: 0.3)
+        let unzoomAction = SKAction.scale(to: 1.0, duration: 0.1)
+        let sequenceAction = SKAction.sequence([zoomAction, unzoomAction])
+    }
+}
--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
-
-
+    
 }
+
+    func randomColor() -> UIColor {
+        let red = CGFloat.random(in: 0.25...0.75)
+        let green = CGFloat.random(in: 0.25...0.75)
+        let blue = CGFloat.random(in: 0.25...0.75)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 
