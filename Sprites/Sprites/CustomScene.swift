c3191e0dc358207d4c369fe94b2589c361b2940d
Sameera Leola <sameeraleola@gmail.com>
2018-11-13 14:54:30 -0600

    Completed the challenge project

---
+++ Sprites/Sprites/CustomScene.swift
@@ -0,0 +1,24 @@
+//
+//  CustomScene.swift
+//  Sprites
+//
+//  Created by Sameera Leola on 11/13/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class CustomScene: SKScene {
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first
+            else { return }
+        
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .red
+        node.position = touch.location(in: self)
+        addChild(node)
+        
+        // Work with your touch here
+    }
+}
--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
+    
+    var skscene: CustomScene? = nil
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var skview: SKView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
 
 
 }
-
