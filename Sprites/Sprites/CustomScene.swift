9e64d7744f1d78377d7b6e67d0cbfb1772138959
Vijay Das <vijaytdas@gmail.com>
2018-11-13 18:49:08 -0600

    cleaned up code comments, moved random color function outside outside func per Dillon, made some setting tweaks to make nodes more fun to watch

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -8,34 +8,30 @@ class CustomScene: SKScene {
 
         guard !touches.isEmpty, let touch = touches.first
             else { return }
-        func randomColor() -> UIColor {
-            let red = CGFloat.random(in: 0.5 ... 1.0)
-            let green = CGFloat.random(in: 0.5 ... 1.0)
-            let blue = CGFloat.random(in: 0.5 ... 1.0)
-            
-            return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
-        }
-        
+  
         
         let node = SKShapeNode(circleOfRadius: 8)
         addChild(node)
         node.fillColor = randomColor()
         node.position = (CGPoint(x: frame.midX, y: frame.midY))
-        // record touchposition
-//        node.location(CGPoint(x: frame.midX, y: frame.midY))
+
         let touchPosition = touch.location(in: self)
 
-//        node.position = touch.location(in: self)
-
-//        let centerAction = node.location(CGPoint(x: frame.midX, y: frame.midY))
-//        let touchPositionAction = node.position(touchPosition)
         let actionDuration = 1.0
         let moveAction = SKAction.move(to: touchPosition, duration: actionDuration)
-        let zoomAction = SKAction.scale(by: 1.3, duration: 0.3)
-        let unzoomAction = SKAction.scale(by: 1.0, duration: 1.0)
+        let zoomAction = SKAction.scale(by: 2.4, duration: 0.3)
+        let unzoomAction = SKAction.scale(by: 0.5, duration: 0.3)
         let sequenceAction = SKAction.sequence([moveAction, zoomAction, unzoomAction, moveAction])
         node.run(sequenceAction)
 
  }
+    
+    func randomColor() -> UIColor {
+        let red = CGFloat.random(in: 0.5 ... 1.0)
+        let green = CGFloat.random(in: 0.5 ... 1.0)
+        let blue = CGFloat.random(in: 0.5 ... 1.0)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 
 }
