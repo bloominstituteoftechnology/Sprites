d6db6fe1c8f7fac26574a1266409917bd2d5602f
Angel <angelbuenrostro@outlook.com>
2019-01-07 18:11:24 -0500

    Add variables for finding the screen dimensions so as to set the initial positions of each added node to the center of the view then add another sequence which changes the position over time to the touch coordinates.

--- Sprites/CustomScene.swift
+++ Sprites/CustomScene.swift
@@ -14,14 +14,22 @@ class CustomScene: SKScene {
         guard !touches.isEmpty, let touch = touches.first
             else { return }
         
+        let bounds = UIScreen.main.bounds
+        let width = bounds.size.width
+        let height = bounds.size.height
+        
         let node = SKShapeNode(circleOfRadius: 8)
         node.fillColor = randomColor()
-        node.position = touch.location(in: self)
+        node.position = CGPoint(x: width/2, y: height/2)
+        //node.position = touch.location(in: self)
         addChild(node)
         
         let scaleUp = SKAction.scale(to: 1.3, duration: 0.5)
         let scaleDown = SKAction.scale(to: 1.0, duration: 0.5)
+        let position = SKAction.move(to: touch.location(in: self), duration: 1.0)
         let sequence = SKAction.sequence([scaleUp, scaleDown])
+        let moveSequence = SKAction.sequence([position])
+        node.run(moveSequence)
         node.run(sequence)
     }
     
