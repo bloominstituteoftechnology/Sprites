f63edd4fe4f7bade217ffe8421841c50bf43f517
Michael R.F <mrflowers22@uwalumni.com>
2019-01-07 17:57:46 -0500

    add customeScene.swift file to the project

---
+++ Sprites/Sprites/CustomScene.swift
@@ -0,0 +1,19 @@
+//
+//  CustomScene.swift
+//  Sprites
+//
+//  Created by Michael Flowers on 1/7/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+class CustomScene: SKScene {
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first else { return }
+        
+    }
+}
--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mySkview: SKView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
