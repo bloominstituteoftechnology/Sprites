457bd5d1429252be66e24d758a789285b1fd9e05
Angel <angelbuenrostro@outlook.com>
2019-01-07 17:25:18 -0500

    Added a custom SKview and code to create red dots whenever the screen is touched.

---
+++ Sprites/CustomScene.swift
@@ -0,0 +1,22 @@
+//
+//  CustomScene.swift
+//  Sprites
+//
+//  Created by Angel Buenrostro on 1/7/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class CustomScene: SKScene {
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first
+            else { return }
+        
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .red
+        node.position = touch.location(in: self)
+        addChild(node)
+    }
+}
