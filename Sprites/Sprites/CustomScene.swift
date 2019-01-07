bd9dc87489213f9f934e2a2017a6b79085c18aeb
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-07 18:18:47 -0500

    add random color and zoom

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -10,14 +10,33 @@ import UIKit
 import SpriteKit
 
 class CustomScene: SKScene {
+   
+    
+
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard !touches.isEmpty, let touch = touches.first
             else { return }
         
         // Work with your touch here
-        let node = SKShapeNode(circleOfRadius: 8)
-        node.fillColor = .red
-        node.position = touch.location(in: self)
+        
+       let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .random
+       node.position = touch.location(in: self)
+        
+      
+        let zoomInAction = SKAction.scale(to: 0.5, duration: 1)
+        node.run(zoomInAction)
         addChild(node)
+    }
+    
+    
+}
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random(in: 0.5...1),
+                       green: .random(in: 0.5...1),
+                       blue: .random(in: 0.5...1),
+                       alpha: 1.0)
     }
 }
