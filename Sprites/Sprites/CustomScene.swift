918fe9892a153a52cf96c24159fbc468021b5141
Franck Tchouambou <franck.tchouambou@gmail.com>
2019-01-08 11:40:05 +0200

    added random colors

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -11,13 +11,26 @@ import Foundation
 import UIKit
 import SpriteKit
 
+extension CGFloat {
+    static var random: CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random, green: .random, blue: .random, alpha: 1.0)
+    }
+}
+
+
 class CustomScene: SKScene {
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard !touches.isEmpty, let touch = touches.first
             else { return }
         
         let node = SKShapeNode(circleOfRadius: 8)
-        node.fillColor = .red
+        node.fillColor = .random
         node.position = touch.location(in: self)
         addChild(node)
     }
