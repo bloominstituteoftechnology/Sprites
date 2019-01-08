80c020ab73cd45baa45d57564d69b90b9fa652ce
Diante Lewis-Jolley <diantelewis-jolley@diantes-macbook-pro.local>
2019-01-07 18:08:45 -0600

    I added nodes to display on the scrren for every touch

--- theRealSprite/theRealSprite/ViewController.swift
+++ theRealSprite/theRealSprite/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
+    var skScene: CustomScene? = nil
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skScene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skScene)
     }
 
+
+   
+    @IBOutlet weak var skview: SKView!
+    
 
 }
 
